350563d4bde367183b8f35d9874649ed70560631
lesley_w <lrwesley92@gmail.com>
2022-02-03 14:33:14 -0600

    Added ability to open and close cart with button click

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//Variables
+
+
+//Selects main image on desktop browser
+const mainImage = document.querySelector(".highlighted-image");
+
+//Selects lightbox_active
+const lightbox = document.querySelector(".lightbox");
+
+//Selects close icon for lightbox
+
+const lightboxClose = document.querySelector(".lightbox_header img");
+
+//Selects cart icon
+
+const cartIcon = document.querySelector(".cart-div");
+
+//Selects actual cart
+
+const cart = document.querySelector(".cart");
+
+//============================================================
+
+//Adds ability to open cart when cart div is clicked
+
+cartIcon.addEventListener("click", function() {
+  if (cart.classList.contains("hide")) {
+    cart.classList.remove("hide");
+  } else {
+    cart.classList.add("hide");
+  }
+})
+
+
+//============================================================
+
+//Adds ability to open and close lightbox
+
+mainImage.addEventListener("click", function() {
+  lightbox.classList.remove("hide");
+})
+
+lightboxClose.addEventListener("click", function() {
+  lightbox.classList.add("hide");
+})
+
+//===========================================================
