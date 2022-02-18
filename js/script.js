e5241b17ded410fdb654c967358d2766cb55431d
lesley_w <lrwesley92@gmail.com>
2022-02-17 23:20:00 -0600

    Still working on cart function. Converted input to number data type

--- js/script.js
+++ js/script.js
@@ -257,34 +257,33 @@ minus.addEventListener("click", function() {
 
 //============================================================
 
-addToCartButton.addEventListener("click", function() {
-  if (quantityInput.value === 0) {
+
+//============================================================
+
+addToCartButton.addEventListener("click", function () {
+  if (quantityInput.value > 0) {
     if (cartQuantity > 0) {
-      cartQuantity = quantityInput.value;
-      quantityInput.value = 0;
-      console.log(cartQuantity);
-
-      cartQuantityDiv.classList.remove("hide");
-      cartQuantityDiv.innerText = `${cartQuantity}`;
-
-      emptyMessage.classList.add("hide");
-      fullCartContent.classList.remove("hide");
-      checkoutButton.classList.remove("hide");
-
-      cartQuantitySpan.innerText = `${cartQuantity}`;
-      totalPrice.innerText = `$${125 * cartQuantity}.00`;
-    } else {
-      let newCartQuantity = (cartQuantity + quantityInput.value);
-      quantityInput.value = 0;
-
+      let newCartQuantity = (cartQuantity + Number(quantityInput.value));
       cartQuantityDiv.innerText = `${newCartQuantity}`;
       cartQuantitySpan.innerText = `${newCartQuantity}`;
       totalPrice.innerText = `$${125 * newCartQuantity}.00`;
     }
+    //!!Need to fix the following line!!
+    cartQuantity = Number(quantityInput.value);
+    quantityInput.value = 0;
+    console.log(typeof(cartQuantity));
+
+    cartQuantityDiv.classList.remove("hide");
+    cartQuantityDiv.innerText = `${cartQuantity}`;
+
+    emptyMessage.classList.add("hide");
+    fullCartContent.classList.remove("hide");
+    checkoutButton.classList.remove("hide");
+
+    cartQuantitySpan.innerText = `${cartQuantity}`;
+    totalPrice.innerText = `$${125 * cartQuantity}.00`;
   }
 })
-
-
 
 //============================================================
 
@@ -293,9 +292,9 @@ addToCartButton.addEventListener("click", function() {
 //addToCartButton.addEventListener("click", function () {
   //if (quantityInput.value > 0) {
     //!!Need to fix the following line!!
-    //cartQuantity = quantityInput.value;
+    //cartQuantity = Number(quantityInput.value);
     //quantityInput.value = 0;
-    //console.log(cartQuantity);
+    //console.log(typeof(cartQuantity));
 
     //cartQuantityDiv.classList.remove("hide");
     //cartQuantityDiv.innerText = `${cartQuantity}`;
